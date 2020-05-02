1724f2539ad2b53584450c0f4242fd902aac9613
Ville Saukkonen <villesau@msn.com>
2020-05-03 01:24:47 +0300

    Drop support for multiple limiters

--- tests/limiter.test.js
+++ tests/limiter.test.js
@@ -55,32 +55,4 @@ describe('Limiter tests', function () {
       done();
     });
   });
-  it('Multiple limiters, tasks should execute in parallel', function (done) {
-    for (var i = 0; i < 5; i++) {
-      c.queue({ uri: 'http://nockHost/status/200', limiter: i });
-    }
-    c.on('drain', function () {
-      expect(tsArrs.length).to.equal(5);
-      // setTimeout in nodejs is delayed
-      // request sent almost at same time
-      expect(tsArrs[4] - tsArrs[0]).to.be.most(50);
-
-      done();
-    });
-  });
-  it('Multiple limiters are mutual independent', function (done) {
-    for (var i = 0; i < 5; i++) {
-      var limiter = i === 4 ? 'second' : 'default';
-      c.queue({ uri: 'http://nockHost/status/200', limiter: limiter });
-    }
-    c.on('drain', function () {
-      expect(tsArrs.length).to.equal(5);
-      // setTimeout in nodejs is delayed
-      // 3 rateLimit +- 50ms = 3 * 500 +- 50
-      expect(tsArrs[4] - tsArrs[0]).to.be.least(1450);
-      expect(tsArrs[4] - tsArrs[0]).to.be.most(1550);
-
-      done();
-    });
-  });
 });
